6c7267c8fe06b0d41d4965bf1713d2e355a8825b
Eunice Oluwatobi Osuagwuh <eunicecloud9@gmail.com>
2026-04-25 00:04:34 +0100

    feat: initial capstone infrastructure

--- terraform/main.tf
+++ terraform/main.tf
@@ -47,7 +47,7 @@ resource "azurerm_kubernetes_cluster" "main" {
   default_node_pool {
   name       = "default"
   node_count = 1
-  vm_size    = "Standard_A2_v2"
+  vm_size    = "Standard_B2s_v2"
 }
 
   identity {
